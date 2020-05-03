1e272f7764efc9f91e944f06c351ae9271363a15
lwsblog <Weisong_Liu@outlook.com>
2020-05-03 09:17:07 +0800

    Site updated: 2020-05-03 09:17:02

---
+++ dist/music.js
@@ -0,0 +1,23 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+	{
+        		name: "Lifeline",
+        		artist: 'Zeraphym 六翼使徒',
+        		url: 'http://tool.liumingye.cn/music/dl/yqd/id/4da2uMTw3fEp1hyLM6kHUdhduoi5l4TSborUF1cVsyQ8lCg4vg/format/320000',
+        		cover: 'https://p1.music.126.net/rukrV8tgLCltzzT7ZJJ6Yw==/109951163861234431.jpg',
+      },{
+    		name: "Fences",
+        		artist: 'Vicetone,Matt Wertz',
+        		url: 'http://tool.liumingye.cn/music/dl/yqd/id/c29c-tUTq6qs7OZIXVcef5hL-YOJh71TgZlV9v97TixjwKKUCJOL/format/320000',
+        		cover: 'https://p2.music.126.net/vyFZN3UxyLCv0cAbQPwV2w==/109951163789368049.jpg',
+      },{
+        		name: "Something Strange",
+        		artist: 'Vicetone,Haley Reinhart',
+        		url: 'http://tool.liumingye.cn/music/dl/yqd/id/28314bIyeOA4eA89KnwOjY2HjV-vHfN8k_nqzFb9QPEhKUxg9jjv/format/320000',
+        		cover: 'https://p2.music.126.net/PtXdUP_SauZtgNoORRAX0A==/109951163628119319.jpg',
+      },
+    ]
+});
